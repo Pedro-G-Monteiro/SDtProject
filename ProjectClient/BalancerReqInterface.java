feb378df2bf719d74061a6d1903da0eb9bc3d1ef
Pedro Monteiro <83717634+PedroGMonteiro@users.noreply.github.com>
2022-11-06 19:37:27 +0000

    Revert "Sprint 2 - Final (Processor's creation and client final implementation)"
    This reverts commit 94ce39d69d18881e2742a102f8ed2ac4e61d9f43.

--- ProjectClient/BalancerReqInterface.java
+++ ProjectClient/BalancerReqInterface.java
@@ -1,3 +1,8 @@
-public interface BalancerReqInterface {
-    String getProcessor(String IDClient);
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public interface BalancerReqInterface extends Remote{
+    ArrayList<String> submitRequest(String script, UUID FileID) throws RemoteException;
 }
