448a6740506ff3bfa35d6f89c366f15cf5ded33b
Pedro Monteiro <83717634+Pedro-G-Monteiro@users.noreply.github.com>
2023-02-04 22:01:32 +0000

    Delete ProjectClient directory

--- ProjectClient/BalancerReqInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.UUID;
-
-public interface BalancerReqInterface extends Remote{
-    ArrayList<String> submitRequest(String script, UUID FileID) throws RemoteException;
-}
